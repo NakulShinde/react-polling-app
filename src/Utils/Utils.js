bb619c79a263fd6d40850231be883e5e612fe474
NakulShinde <nakulshinde.it@gmail.com>
2018-11-29 09:19:14 +0530

    Hadeling NaN codition in percentage calculation by checking either vote or totalVotes are zero

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -6,7 +6,7 @@ export const getQuestionTotalVotes = (choices) => {
 
 export const votePercentage = (vote, totalVotes) => {
     try{
-        return ((vote/totalVotes)*100).toFixed(2)
+        return (vote == 0 || totalVotes == 0)? 0 : ((vote/totalVotes)*100).toFixed(2)
     }catch(e){
         console.log(e.message);
         return 'N/A';
