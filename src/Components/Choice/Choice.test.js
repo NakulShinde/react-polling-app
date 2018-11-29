3be1e0bf08761a7ec3fbec2e491b9a8ca1a06622
NakulShinde <nakulshinde.it@gmail.com>
2018-11-29 13:30:16 +0530

    removing <MemoryRouter> from Choices & Adding test case to check onVoteSelect

--- src/Components/Choice/Choice.test.js
+++ src/Components/Choice/Choice.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import renderer from "react-test-renderer";
 import {shallow} from 'enzyme';
-import {MemoryRouter} from "react-router-dom";
 
 import Choice from './Choice';
 
 // create props to initialize
 const props = {
+    onVoteSelect: jest.fn(),
     choice: {
         "url": "/questions/9/choices/65",
         "votes": 246,
@@ -15,11 +15,7 @@ const props = {
 }
 let wrapper;
 beforeEach(() => {
-    wrapper = shallow(
-        <MemoryRouter>
-            <Choice {...props}/>
-        </MemoryRouter>
-    )
+    wrapper = shallow(<Choice {...props}/>)
 })
 
 it("renders Choice snapshot correctly", () => {
@@ -27,4 +23,14 @@ it("renders Choice snapshot correctly", () => {
         .create(wrapper)
         .toJSON();
     expect(tree).toMatchSnapshot();
+});
+
+it("Choice input onchage called on onVoteSelect", () => {
+
+    const input = wrapper.find('input');
+    expect(input.length).toBe(1);
+
+    input.simulate('change', {})
+    expect(props.onVoteSelect).toHaveBeenCalled();
+
 });
